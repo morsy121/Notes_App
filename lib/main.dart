96fcdab2e1a1fd8a6933de7cb021e629ae4cec6e
morsy121 <morsyhashish123@gmail>
2022-11-03 15:16:50 -0700

    create notes app

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:notesapp/views/notes_view.dart';
+
+void main() {
+  runApp(const NotesApp());
+}
+
+class NotesApp extends StatelessWidget {
+  const NotesApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(brightness: Brightness.dark),
+      debugShowCheckedModeBanner: false,
+      home: const NotesView(),
+    );
+  }
+}
